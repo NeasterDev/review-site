8f66cd8e9a8a1d9d75b6d985735c85fba72c67ee
Nicholas Easter <nicholas.easter17@gmail.com>
2022-04-21 12:57:39 -0700

    added server config for server.js files and installed the bcrypt and jsonwebtoken packages

--- server/server.js
+++ server/server.js
@@ -1,7 +1,35 @@
+const express = require('express');
+const db = require('./config/connection');
+const {ApolloServer} = require('apollo-server-express');
+const {typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: authMiddleware
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const startServer = async () => {
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        context: authMiddleware
+    });
+
+    await server.start();
+    server.applyMiddleware({ app });
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+startServer();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json);
+
+
+// once we are in production use build folder that react creates
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+};
+
+db.once('open', () => {
+    app.listen(PORT, () => console.log(` Now listening on localhost:${PORT}`));
 });
+
