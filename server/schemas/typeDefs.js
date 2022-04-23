aafbc9ba38191b7b044f76abd6169ce6aa5748b9
Nicholas Easter <nicholas.easter17@gmail.com>
2022-04-23 12:43:43 -0700

    add review and delete review functions were outside mutation object

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -34,7 +34,7 @@ const typeDefs = gql`
         login(email: String!, password: String!): Auth
         updateUser(username: String!, password: String!): Auth
         removeUser(_id: ID!): Auth
-        addReview(review_text: String!, rating: Int!): Review
+        addReview(review_text: String!, rating: Int!): User
         deleteReview(_id: ID!): Review
     }
 `;
