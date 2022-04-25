0e5841eb707a461b35297dfb68a908b12dbcce01
Coder J <buildingguy@sbcglobal.net>
2022-04-24 17:33:42 -0700

    set up basic wireframe for React client

---
+++ client/src/components/ReviewContainer/index.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import NavTabs from '../NavTabs/index';
+import Home from '../pages/Home/index';
+import Login from '../pages/Login/index';
+import Signup from '../pages/Signup/index';
+import Write from '../pages/Write/index';
+import './style.css';
+
+function ReviewContainer() {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  const renderPage = () => {
+    if (currentPage === 'Home') {
+      return <Home />;
+    }
+    if (currentPage === 'Login') {
+      return <Login />;
+    }
+    if (currentPage === 'Signup') {
+      return <Signup />;
+    }
+    return <Write />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <header>
+        <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      </header>
+      <main>
+        {renderPage()}
+      </main>
+    </div>
+  );
+}
+
+export default ReviewContainer;
---
+++ client/src/components/pages/Signup/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+export default function Signup() {
+  return (
+    <div className="signup">
+        <h1>Signup</h1>
+    </div>
+  );
+}
