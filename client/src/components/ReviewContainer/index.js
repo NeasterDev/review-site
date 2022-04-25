c3e1a93c3ba15db991b953342449cfd9a428cd0c
Coder J <buildingguy@sbcglobal.net>
2022-04-25 12:15:21 -0700

    modify ReviewContainer file

--- client/src/components/ReviewContainer/index.js
+++ client/src/components/ReviewContainer/index.js
@@ -1,38 +1,38 @@
 import React, { useState } from 'react';
+
+// component root
 import NavTabs from '../NavTabs/index';
 import Footer from '../Footer/index';
+
+// pages
 import Home from '../pages/Home/index';
 import Login from '../pages/Login/index';
 import Signup from '../pages/Signup/index';
 import Write from '../pages/Write/index';
+
+//style sheet
 import './style.css';
 
+// packages
+import { ApolloProvider } from '@apollo/client';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 function ReviewContainer() {
-  const [currentPage, setCurrentPage] = useState('Home');
-
-  const renderPage = () => {
-    if (currentPage === 'Home') {
-      return <Home />;
-    }
-    if (currentPage === 'Login') {
-      return <Login />;
-    }
-    if (currentPage === 'Signup') {
-      return <Signup />;
-    }
-    return <Write />;
-  };
-
-  const handlePageChange = (page) => setCurrentPage(page);
-
   return (
-    <div>
-      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
-      <main>
-        {renderPage()}
-      </main>
-      <Footer></Footer>
-    </div>
+    <ApolloProvider>
+      <Router>
+        <div>
+          <NavTabs></NavTabs>
+          <main>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/write" component={Write} />
+          </main>
+          <Footer></Footer>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
