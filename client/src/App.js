3a57fc8e512124adde5b9218172cf134610e1233
Nicholas Easter <nicholas.easter17@gmail.com>
2022-04-25 22:47:16 -0700

    commit before pull

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import ReviewContainer from './components/ReviewContainer/index';
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import React from "react";
+import ReviewContainer from "./components/ReviewContainer/index";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div>
-      <ReviewContainer></ReviewContainer>
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        <ReviewContainer></ReviewContainer>
+      </div>
+    </ApolloProvider>
   );
 }
 
