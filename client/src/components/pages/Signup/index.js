bb2ec60a3ca984539afd147d0cb016b472e7899b
Coder J <buildingguy@sbcglobal.net>
2022-04-25 21:35:03 -0700

    create signip form

--- client/src/components/pages/Signup/index.js
+++ client/src/components/pages/Signup/index.js
@@ -3,8 +3,27 @@ import './style.css';
 
 export default function Signup() {
   return (
-    <div className="signup">
-        <h1>Signup</h1>
+    <div class="signup mt-6 ml-5">
+        <div class="field">
+            <label class="label">Name</label>
+            <div class="control">
+              <input class="input" type="text" placeholder="e.g Alex Smith" />
+            </div>
+          </div>
+          
+          <div class="field">
+            <label class="label">Email</label>
+            <div class="control">
+              <input class="input" type="email" placeholder="e.g. alexsmith@gmail.com" />
+            </div>
+          </div>
+
+          <div class="field">
+            <label class="label">Password</label>
+            <div class="control">
+              <input class="input" type="password" />
+            </div>
+          </div>
     </div>
   );
 }
