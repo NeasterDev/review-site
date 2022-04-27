ea563dc7609bd7baf15e528bda7ef15d9bd92d06
Coder J <buildingguy@sbcglobal.net>
2022-04-26 18:55:09 -0700

    profile page setup

--- client/src/components/pages/Profile/index.js
+++ client/src/components/pages/Profile/index.js
@@ -5,6 +5,22 @@ export default function Profile() {
   return (
     <div className="profile">
         <h1>Profile</h1>
+        <div className="column">
+          <div className="column is-three-fifths">
+            <text>[image goes here]</text>
+          </div>
+          <div className="column is-full">
+            <h1>Bio</h1>
+          </div>
+        </div>
+        <div className="column">
+          <div className="column is-two-fifths">
+            <h2>Reviews</h2>
+          </div>
+          <div className="column is-two-fifths">
+            <h2>Edit</h2>
+          </div>
+        </div>
     </div>
   );
 }
