82c815aceb56e62ef28c5f9fea71725834626be6
Elisa Vidal <eunice1vidal@yahoo.com>
2022-04-28 16:30:35 -0700

    save

--- client/src/components/pages/Login/index.js
+++ client/src/components/pages/Login/index.js
@@ -40,12 +40,7 @@ const Login = (props) => {
   };
 
   return (
-<<<<<<< HEAD
-    
-    <main className="flex-row justify-center mb-4">
-=======
     <div className="flex-row justify-center mb-4 mt-8">
->>>>>>> ccc6eda40bd074bbfd60d5a7903f92f89fc1be59
       <div className="col-12 col-md-6">
         <div className="card">
           <h4 className="card-header">Login</h4>
