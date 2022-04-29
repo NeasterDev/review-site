02ae6ac0be7bf39a833b720930709fd9004aef48
Nicholas Easter <nicholas.easter17@gmail.com>
2022-04-28 19:08:36 -0700

    merging to main

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import "./style.css";
 
 export default function Footer() {
   return (
-    <footer className="has-background-success">
-      <p className="title"></p>
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <div className="is-flex is-justify-content-space-between">
+          <div><strong>Nelp</strong> by{" "}</div>
+          <div><a className="title" href="?">Nicholas Easter </a></div>
+          <div><a className="title" href="?">Joshua Jones </a></div>
+          <div><a className="title" href="?">Elisa Vidal</a></div>
+        </div>
+      </div>
     </footer>
   );
 }
