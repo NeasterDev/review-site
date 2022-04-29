1cdf243a65cd037c010c4294c95170166d38666b
Elisa Vidal <eunice1vidal@yahoo.com>
2022-04-28 17:21:27 -0700

    save

--- client/src/components/pages/Profile/index.js
+++ client/src/components/pages/Profile/index.js
@@ -1,39 +1,53 @@
 import React from "react";
 import "./style.css";
 import Write from "../../Write/index";
-import { QUERY_REVIEW, QUERY_GET_ME } from "../../../utils/query";
+import { QUERY_GET_ME} from "../../../utils/query";
+import { ADD_REVIEW} from '../../../utils/mutations';
 import Auth from "../../../utils/auth";
+import { useState } from "react";
+
+export const QUERY_GET_ME = ({location, rating, reviewText}) => {
+    const [reviewText, setText] = useState('');
+    
+    const [addReview, { error }] = useMutation(QUERY_GET_ME, {
+      update(cache, { data: { addReview} }) {
+        try {
+          const { review } = cache.readQuery({ query: QUERY_GET_ME});
+          cache.writeQuery({
+            query: QUERY_GET_ME,
+            data: { thoughts: [addReview, ...review] },
+          });
+        } catch (e) {
+          console.error(e);
+        }
+};
+    };
 
 
-export const Review = ({location, rating, reviewText, username}) => {
-  
-}
 
 
-
-
-  export default function Profile() {
     return (
       <div className="profile">
-        <h1>Profile</h1>
-        <div className="column">
-          <div className="column is-three-fifths is-one-fifth-desktop">
-            <p>[image goes here]</p>
+          <h1>Profile</h1>
+          <div className="column">
+            <div className="column is-three-fifths is-one-fifth-desktop">
+              <p>[image goes here]</p>
+            </div>
+            <div className="column is-full is-three-fifths-desktop">
+              <h2>Bio</h2>
+            </div>
           </div>
-          <div className="column is-full is-three-fifths-desktop">
-            <h2>Bio</h2>
+          <div className="column">
+            <div className="column is-two-fifths is-three-fifths-desktop">
+              <h2>Reviews</h2>
+            </div>
+            <div className="column is-two-fifths is-three-fifths-desktop">
+              <h2>Edit</h2>
+            </div>
           </div>
-        </div>
-        <div className="column">
-          <div className="column is-two-fifths is-three-fifths-desktop">
-            <h2>Reviews</h2>
-          </div>
-          <div className="column is-two-fifths is-three-fifths-desktop">
-            <h2>Edit</h2>
-          </div>
-        </div>
-        <Write></Write>
+          <Write></Write>
       </div>
     );
   }
-
+  
+  export default Profile;
